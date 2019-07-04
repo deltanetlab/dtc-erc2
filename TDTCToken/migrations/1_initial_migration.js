79e265fc84ad3dedfab05663df96cd7a25c1eaec
xbee <hfeeki@outlook.com>
2019-07-04 11:20:43 +0800

    rm tdtc

--- TDTCToken/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-
-module.exports = function (deployer) {
-    // console.log(Migrations);
-    console.log(deployer.provider);
-    deployer.deploy(Migrations);
-};
